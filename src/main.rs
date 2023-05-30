ef6a5a11809b1a92cf0b5d8be270fc2873c9d884
amachang <seijro@gmail.com>
2023-05-30 10:21:19 +0900

    simple main funciton

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,47 @@ use fq::{
     evaluate,
 };
 
+use nom::{
+    Err,
+    // error::convert_error,
+};
+
+use clap::{
+    Parser,
+};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    query: String,
+}
+
 fn main() {
-    for source_str in Vec::from(["/", " /", " / ", " .", "-30", "-30 ", "-30A"]) {
-        match parse(source_str) {
-            Ok(expr) => {
-                println!("Parsed {:?}: {:?}", source_str, evaluate(&*expr));
-            },
-            Err(e) => println!("Parsed {:?}: {:?}", source_str, e),
-        };
+    let args = Args::parse();
+    let query = &args.query;
+    let result = parse(query);
+
+    let expr = match result {
+        Err(Err::Incomplete(_)) => unreachable!(),
+        Err(Err::Error(e)) | Err(Err::Failure(e)) => {
+            // println!("Parse Error: \n{}", convert_error(query, e));
+            println!("Parse Error: {}", e);
+            return ();
+        },
+        Ok(expr) => expr,
+    };
+
+    let result = evaluate(&*expr);
+    let values = match result {
+        Err(e) => {
+            println!("Excution Error: {:?}", e);
+            return ();
+        },
+        Ok(values) => values,
+    };
+    for value in &values {
+        let line: String = value.into();
+        println!("{}", line);
     }
 }
 
